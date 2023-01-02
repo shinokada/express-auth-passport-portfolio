3a664f0562c8fa71cb8da9228af5b37b7476a8c7
Shinichi Okada <okada.shin@gmail.com>
2023-01-03 05:54:44 +0900

    > Gitignore updated at 2023-01-03 05:54:44

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,41 +1,31 @@
 import express from 'express'
 import Debug from 'debug'
-// import mongoose from 'mongoose'
-import pkg from 'mongodb';
-const { MongoClient } = pkg;
-// you need to add assert { type: "json" } to import JSON in ES modules
-import articles from '../data/mydata.json' assert { type: "json" }
-// import { Article } from './articleModel.js'
-
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import Upstash from 'upstash';
+import * as dotenv from 'dotenv'
 dotenv.config()
 
 const debug = Debug('app:adminRouter')
 const adminRouter = express.Router()
 
-// mongoose.set('strictQuery', true);
+const url = process.env.UPSTASH_REDIS_REST_URL
+const token = process.env.UPSTASH_REDIS_REST_TOKEN
 
-const username = process.env.USERNAME
-const password = process.env.PASSWORD
-const cluster = process.env.CLUSTERNAME
+const upstash = new Redis({
+  url,
+  token,
+});
 
 adminRouter.route('/').get(async (req, res) => {
-  const dbName = 'demo1'
-  const url = `mongodb+srv://${username}:${password}@${cluster}.sabpl.mongodb.net/?retryWrites=true&w=majority`
-
-  debug('Connecting DB ...')
-  const client = await MongoClient.connect(url)
-  const db = client.db(dbName)
-  const collection = db.collection('articles')
-
   try {
-    const response = await collection.insertMany(articles)
+    const response = await upstash.putObject({
+      Bucket: bucketName,
+      Key: 'mydata.json',
+      Body: articles,
+    });
     debug("Data inserted")
     res.json(response)
   } catch (error) {
     debug('error', error)
-  } finally {
-    client.close()
   }
 })
 
