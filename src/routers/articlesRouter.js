2a24dab5e3ba8390642d17e80955817135a2c64a
Shinichi Okada <okada.shin@gmail.com>
2023-01-02 17:20:17 +0900

    update selecting one article

--- src/routers/articlesRouter.js
+++ src/routers/articlesRouter.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import Debug from 'debug'
 import pkg from 'mongodb';
-const { MongoClient } = pkg;
+const { MongoClient, ObjectID } = pkg;
 // you need to add assert { type: "json" } to import JSON in ES modules
 // import articles from '../data/mydata.json' assert { type: "json" }
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
@@ -35,12 +35,27 @@ articlesRouter.route('/').get(async (req, res) => {
   }
 })
 // articles/any-id
-articlesRouter.route('/:id').get((req, res) => {
+articlesRouter.route('/:id').get(async (req, res) => {
   const id = req.params.id
-  res.render('article', {
-    article: articles[id]
+  // res.render('article', {
+  //   article: articles[id]
+  // })
+  const dbName = 'demo1'
+  const url = `mongodb+srv://${username}:${password}@${cluster}.sabpl.mongodb.net/?retryWrites=true&w=majority`
+
+  debug('Connecting DB ...')
+  const client = await MongoClient.connect(url)
+  const db = client.db(dbName)
+  const collection = db.collection('articles')
+
+  try {
+    const article = await collection.findOne({ _id: new ObjectID(id) })
+    res.render('article', { article })
+  } catch (error) {
+    debug('error', error)
+  } finally {
+    client.close()
   }
-  )
 })
 
 export { articlesRouter }
