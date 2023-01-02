9283b12bd1450f1f1277702fa5a06d4ad5e7945a
Shinichi Okada <okada.shin@gmail.com>
2023-01-02 16:59:55 +0900

    change without mongoose

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,57 +1,51 @@
 import express from 'express'
 import Debug from 'debug'
-import { default as mongodb } from 'mongodb';
-let MongoClient = mongodb.MongoClient;
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
+import pkg from 'mongodb';
+const { MongoClient } = pkg;
 // you need to add assert { type: "json" } to import JSON in ES modules
 import articles from '../data/mydata.json' assert { type: "json" }
+// import { Article } from './articleModel.js'
+
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
 const debug = Debug('app:adminRouter')
 const adminRouter = express.Router()
 
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
 
 const username = process.env.USERNAME
 const password = process.env.PASSWORD
 const cluster = process.env.CLUSTERNAME
 
-adminRouter.route('/').get((req, res) => {
+adminRouter.route('/').get(async (req, res) => {
   const dbName = 'demo1'
   const url = `mongodb+srv://${username}:${password}@${cluster}.sabpl.mongodb.net/?retryWrites=true&w=majority`
 
   debug('Connecting DB ...')
-  mongoose.connect(url, { dbName, useNewUrlParser: true, useUnifiedTopology: true })
+  const client = await MongoClient.connect(url)
+  const db = client.db(dbName)
+  const collection = db.collection('articles')
 
-  // Article model
-  const Article = mongoose.model('Article', {
-    id: { type: Number },
-    title: { type: String },
-    description: { type: String },
-    startsAt: { type: String },
-    endsAt: { type: String },
-    room: { type: String },
-    day: { type: String },
-    format: { type: String },
-    track: { type: String },
-    level: { type: String }
-  });
-  // Function call
-  // Article.insertMany(articles).then(function () {
-  //   debug("Data inserted")  // Success
-  // }).catch(function (error) {
-  //   debug('error', error)      // Failure
-  // })
-  (async function insertArticles () {
-    try {
-      const response = await Article.insertMany(articles);
-      debug("Data inserted");
-      res.json(response)
-    } catch (error) {
-      debug('error', error);
-    }
-  })()
+  try {
+    const response = await collection.insertMany(articles)
+    debug("Data inserted")
+    res.json(response)
+  } catch (error) {
+    debug('error', error)
+  } finally {
+    client.close()
+  }
+  // (async function insertArticles () {
+  //   try {
+  //     const response = await Article.insertMany(articles);
+  //     debug("Data inserted");
+  //     res.json(response)
+  //   } catch (error) {
+  //     debug('error', error);
+  //   }
+  // })()
 })
 
 export { adminRouter }
---
+++ src/routers/articleModel.js
@@ -0,0 +1,18 @@
+// import mongoose from 'mongoose'
+// // const Schema = mongoose.Schema
+
+// // Article model
+// const articleSchema = mongoose.model('articleSchema', {
+//   id: { type: Number },
+//   title: { type: String },
+//   description: { type: String },
+//   startsAt: { type: String },
+//   endsAt: { type: String },
+//   room: { type: String },
+//   day: { type: String },
+//   format: { type: String },
+//   track: { type: String },
+//   level: { type: String }
+// });
+
+// export { articleSchema } 
