ead985d2ad7d51e870901204f1ff28544e8af896
Shinichi Okada <okada.shin@gmail.com>
2023-01-16 06:17:26 +0900

    fix: article page

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -8,35 +8,11 @@ let redis = new Redis(process.env.REDIS_UPSTASH);
 const debug = Debug('app:adminRouter')
 const adminRouter = express.Router()
 
-adminRouter.route('/').get(async (req, res) => {
-  res.render('admin')
+adminRouter.route('/').get((req, res) => {
+  const title = "Admin"
+  res.render('admin', { title })
 })
 
-adminRouter.route('/create').post(async (req, res) => {
-  let { title, description, content } = req.body
 
-  if (!title) {
-    res.status(400).json({
-      error: 'Title can not be empty',
-    })
-  } else if (title.length < 150) {
-    const id = uuidv4()
-    title = title.replace(/\s+/g, '-').toLowerCase();
-    const newEntry = {
-      id,
-      title,
-      created_at: Date.now(),
-      description,
-      content
-    }
-    await redis.hset('articles', title, JSON.stringify(newEntry))
-    let articles = await redis.hgetall('articles');
 
-    res.render('articles', { articles })
-  } else {
-    res.status(400).json({
-      error: 'Max 150 characters please.',
-    })
-  }
-})
 export { adminRouter }
--- src/routers/articlesRouter.js
+++ src/routers/articlesRouter.js
@@ -10,38 +10,79 @@ const articlesRouter = express.Router()
 articlesRouter.route('/').get(async (req, res) => {
   debug('in article route')
   try {
-    // const articles = await collection.find().toArray()
-    const articles = await redis.hgetall('articles');
-    if (articles) {
-      debug('articles: ', articles)
-      // res.send('hi 1')
-      res.render('articles', { articles })
+    let articles = await redis.keys('article:*');
+    let articlesData = await redis.mget(...articles);
+    articles = articlesData.map(JSON.parse);
+    debug('articles: ', articles)
+    const pageTitle = 'Admin: Articles'
+    res.render('articles', { articles, title: pageTitle })
+  } catch (error) {
+    debug('error', error)
+  }
+})
+
+articlesRouter.route('/create').get(async (req, res) => {
+  const pageTitle = 'Create article'
+  res.render('create', { title: pageTitle });
+});
+
+articlesRouter.route('/create').post(async (req, res) => {
+  // Get the form data from the request body
+  const { title, description, content } = req.body;
+  // Create a new article in the database
+  // let article = await createArticle({ title, description, content });
+  // let { title, description, content } = req.body
+
+  if (!title) {
+    res.status(400).json({
+      error: 'Title can not be empty',
+    })
+  } else if (title.length < 150) {
+    const id = uuidv4();
+    const newEntry = {
+      id,
+      title,
+      created_at: Date.now(),
+      description,
+      content
+    }
+    //validate the newEntry
+    if (!newEntry.description) {
+      res.status(400).json({ error: 'Description can not be empty' });
+      return;
+    }
+    if (!newEntry.content) {
+      res.status(400).json({ error: 'Content can not be empty' });
+      return;
+    }
+
+    //stringify the new entry
+    const newEntryAsString = JSON.stringify(newEntry);
+    //save the new entry to redis
+    await redis.set(`article:${id}`, newEntryAsString);
+    // Redirect the user to the article page
+    res.redirect(`/article/${newEntry.id}`);
+  }
+});
+
+// articles/any-id
+articlesRouter.route('/:id').get(async (req, res) => {
+  const id = req.params.id
+  try {
+    debug('Connecting DB ...')
+    let article = await redis.get(`article:${id}`);
+    debug('article: ', article)
+    if (!article) {
+      res.status(404).render('404', { message: `Article with id ${id} not found`, title: '404' });
+    } else {
+      let articleData = JSON.parse(article);
+      const pageTitle = 'Article'
+      res.render('article', { article: articleData, title: pageTitle });
     }
   } catch (error) {
     debug('error', error)
   }
 })
-// articles/any-id
-articlesRouter.route('/:id').get(async (req, res) => {
-  const id = req.params.id
-  // res.render('article', {
-  //   article: articles[id]
-  // })
-  // const dbName = 'demo1'
-  // const url = `mongodb+srv://${username}:${password}@${cluster}.sabpl.mongodb.net/?retryWrites=true&w=majority`
 
-  // const client = await MongoClient.connect(url)
-  // const db = client.db(dbName)
-  // const collection = db.collection('articles')
-
-  try {
-    // const article = await collection.findOne({ _id: new ObjectID(id) })
-    debug('Connecting DB ...')
-    let articles = await redis.hget('articles', id);
-    res.render('article', { article })
-  } catch (error) {
-    debug('error', error)
-  }
-})
 
 export { articlesRouter }
