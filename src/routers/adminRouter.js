49203a4bb36c308347dfdd498cca6c61118e7ba3
Shinichi Okada <okada.shin@gmail.com>
2023-01-15 13:30:05 +0900

    working on articles

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -2,25 +2,17 @@ import express from 'express'
 import Debug from 'debug'
 import { v4 as uuidv4 } from 'uuid'
 import Redis from "ioredis"
-// import { Redis } from '@upstash/redis'
 
-let redis = new Redis("redis://default:c92b3c34cd054cb09e99ef27d52d3d8c@apn1-apt-jennet-33538.upstash.io:33538");
+let redis = new Redis(process.env.REDIS_UPSTASH);
 
 const debug = Debug('app:adminRouter')
 const adminRouter = express.Router()
 
-// const url = process.env.UPSTASH_REDIS_REST_URL
-// const token = process.env.UPSTASH_REDIS_REST_TOKEN
-// console.log('url: ', url)
-
 adminRouter.route('/').get(async (req, res) => {
   res.render('admin')
-  // const member = await redis.srandmember("express")
-  // debug('random member: ', member)
 })
 
 adminRouter.route('/create').post(async (req, res) => {
-
   let { title, description, content } = req.body
 
   if (!title) {
@@ -37,9 +29,7 @@ adminRouter.route('/create').post(async (req, res) => {
       description,
       content
     }
-    // await redis.hset(JSON.stringify(newEntry))
     await redis.hset('articles', title, JSON.stringify(newEntry))
-    // await redis.set('key', 'value');
     let articles = await redis.hgetall('articles');
 
     res.render('articles', { articles })
--- src/routers/articlesRouter.js
+++ src/routers/articlesRouter.js
@@ -1,34 +1,24 @@
 import express from 'express'
 import Debug from 'debug'
-import pkg from 'mongodb';
-const { MongoClient, ObjectID } = pkg;
-// you need to add assert { type: "json" } to import JSON in ES modules
-// import articles from '../data/mydata.json' assert { type: "json" }
+import Redis from "ioredis"
+
+let redis = new Redis(process.env.REDIS_UPSTASH);
 
 const debug = Debug('app:adminRouter')
 const articlesRouter = express.Router()
-const username = process.env.USERNAME
-const password = process.env.PASSWORD
-const cluster = process.env.CLUSTERNAME
 
 articlesRouter.route('/').get(async (req, res) => {
-  // res.render('articles', {
-  //   articles,
-  // })
-  const dbName = 'demo1'
-  const url = `mongodb+srv://${username}:${password}@${cluster}.sabpl.mongodb.net/?retryWrites=true&w=majority`
-
-  debug('Connecting DB ...')
-  const client = await MongoClient.connect(url)
-  const db = client.db(dbName)
-  const collection = db.collection('articles')
+  debug('in article route')
   try {
-    const articles = await collection.find().toArray()
-    res.render('articles', { articles })
+    // const articles = await collection.find().toArray()
+    const articles = await redis.hgetall('articles');
+    if (articles) {
+      debug('articles: ', articles)
+      // res.send('hi 1')
+      res.render('articles', { articles })
+    }
   } catch (error) {
     debug('error', error)
-  } finally {
-    client.close()
   }
 })
 // articles/any-id
@@ -37,21 +27,20 @@ articlesRouter.route('/:id').get(async (req, res) => {
   // res.render('article', {
   //   article: articles[id]
   // })
-  const dbName = 'demo1'
-  const url = `mongodb+srv://${username}:${password}@${cluster}.sabpl.mongodb.net/?retryWrites=true&w=majority`
+  // const dbName = 'demo1'
+  // const url = `mongodb+srv://${username}:${password}@${cluster}.sabpl.mongodb.net/?retryWrites=true&w=majority`
 
-  debug('Connecting DB ...')
-  const client = await MongoClient.connect(url)
-  const db = client.db(dbName)
-  const collection = db.collection('articles')
+  // const client = await MongoClient.connect(url)
+  // const db = client.db(dbName)
+  // const collection = db.collection('articles')
 
   try {
-    const article = await collection.findOne({ _id: new ObjectID(id) })
+    // const article = await collection.findOne({ _id: new ObjectID(id) })
+    debug('Connecting DB ...')
+    let articles = await redis.hget('articles', id);
     res.render('article', { article })
   } catch (error) {
     debug('error', error)
-  } finally {
-    client.close()
   }
 })
 
