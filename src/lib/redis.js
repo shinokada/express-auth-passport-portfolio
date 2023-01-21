d76e4c90f2578ad5e9076562f1ca596676f3dfbb
Shinichi Okada <okada.shin@gmail.com>
2023-01-22 08:18:14 +0900

    feat: add titleExists for creating a project

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -12,4 +12,14 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'developme
   redis = new Redis({ host: redis_host, port: redis_port });
 }
 
+//  Redis Hash to store the titles as keys and their values as 1, and then using the HEXISTS command to check for the existence of a given title in the hash. This would have a O(1) average time complexity for both adding and checking, which is faster than O(n) average time complexity of sismember.
+
+// export const titleExists = async (title) => {
+//   return await redis.sismember("project:projectnames", title);
+// };
+
+export const titleExists = async (title) => {
+  return await redis.hexists("project:titles", title);
+}
+
 export default redis
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -2,35 +2,23 @@ import express from 'express'
 import chalk from 'chalk'
 import Debug from 'debug'
 // import { v4 as uuidv4 } from 'uuid'
-import redis from '../lib/redis.js'
+import redis, { titleExists } from '../lib/redis.js'
 import { replaceSpacesWithDashes } from '../lib/utils.js'
 
 const debug = Debug('app:adminRouter')
 const adminRouter = express.Router()
 
 adminRouter.use((req, res, next) => {
-  try {
-    req.user = JSON.parse(req.user);
-  } catch (e) {
-    console.error("req.user is not a valid json string", e)
-  }
-
   if (req.user) {
     next();
   } else {
     res.redirect('/auth/login');
   }
-  // Make sure the user is an admin
-  // if (!req.user.isAdmin) {
-  //   res.status(401).json({ error: 'Unauthorized' });
-  //   return;
-  // }
 });
 
 
 adminRouter.route('/').get(async (req, res) => {
   let projects = []
-
   if (req.user && req.user.isAdmin) {
     // Get all the project keys from Redis
     const projectKeys = await redis.keys('project:*');
@@ -39,11 +27,9 @@ adminRouter.route('/').get(async (req, res) => {
       return JSON.parse(projectAsString);
     }));
   } else if (req.user) {
-
     const username = req.user.username
     // This will return an array of project ids for the user with the provided username.
     const projectIds = await redis.smembers(`projects:username:${username}`);
-    debug(chalk.green('projectIds: ', projectIds))
 
     for await (const id of projectIds) {
       const project = JSON.parse(await redis.get(`project:slug:${id}`))
@@ -51,21 +37,18 @@ adminRouter.route('/').get(async (req, res) => {
     }
 
   }
-  debug(chalk.green('projects: ', projects))
-  const title = "Profile"
-  // Render the projects page
-  res.render('admin/index', { projects, title });
+
+  res.render('admin/index', { projects });
 });
 
 
 adminRouter.route('/profile').get((req, res) => {
-  const pageTitle = "Profile"
-  res.render('admin/profile', { title: pageTitle, user: JSON.parse(req.user) });
+  res.render('admin/profile', { user: JSON.parse(req.user) });
 });
 
 adminRouter.route('/create-project').get((req, res) => {
-  const pageTitle = 'Create project'
-  res.render('admin/create-project', { title: pageTitle, user: req.user });
+  const error = ''
+  res.render('admin/create-project', { user: req.user, error });
 });
 
 adminRouter.route('/create-project').post(async (req, res) => {
@@ -76,41 +59,85 @@ adminRouter.route('/create-project').post(async (req, res) => {
       error: 'Project name can not be empty',
     })
   } else if (projectName.length < 150) {
-
-    const newEntry = {
-      id: replaceSpacesWithDashes(projectName),
-      projectName,
-      image,
-      created_at: Date.now(),
-      description,
-      content,
-      username
+    const newId = replaceSpacesWithDashes(projectName);
+    // check if the new id already exists in the set of project ids
+    const isTitleExist = await titleExists(newId);
+    if (isTitleExist) {
+      return res.status(409).render('admin/create-project', { user: req.user, error: 'Project name already exists' });
+    } else {
+      const newEntry = {
+        id: newId,
+        projectName,
+        image,
+        created_at: Date.now(),
+        description,
+        content,
+        username
+      }
+      //validate the newEntry
+      if (!newEntry.description || !newEntry.content || !newEntry.image) {
+        return res.status(400).render('admin/create-project', { user: req.user, error: 'Description, Content and Image can not be empty' });
+      }
+      //stringify the new entry
+      const newEntryAsString = JSON.stringify(newEntry);
+      //save the new entry to redis using the projectKey function
+      await redis.set(`project:slug:${newId}`, newEntryAsString);
+      // add the new id to the set of project ids. newId is lowercase, spaces replaced with dashes
+      // this will be used redis/titleExists function
+      await redis.hset("project:titles", newId, 1);
+      // Add the project id to the set keyed by the username
+      // to collect all projects by username
+      await redis.sadd(`projects:username:${newEntry.username}`, newId);
+      // Redirect the user to the project page
+      res.redirect(`/projects/${newId}`);
     }
-
-    //validate the newEntry
-    if (!newEntry.description || !newEntry.content || !newEntry.image) {
-      return res.status(400).render('error', { message: 'Description, Content and Image can not be empty' });
-    }
-
-    //stringify the new entry
-    const newEntryAsString = JSON.stringify(newEntry);
-    //save the new entry to redis using the projectKey function
-    await redis.set(`project:slug:${newEntry.id}`, newEntryAsString);
-
-    // Add the project id to the set keyed by the username
-    // to collect all projects by username
-    await redis.sadd(`projects:username:${newEntry.username}`, newEntry.id);
-
-    // Redirect the user to the project page
-    res.redirect(`/projects/${newEntry.id}`);
   }
 });
 
-adminRouter.route('/:slug').get(async (req, res) => {
+
+adminRouter.route('/project-update/:slug').put(async (req, res) => {
+  const { projectName, description, image, content, username } = req.body;
+  const oldSlug = req.params.slug;
+  const newSlug = replaceSpacesWithDashes(projectName);
+  const newId = replaceSpacesWithDashes(projectName);
+
+  redis.get(`project:slug:${oldSlug}`, (err, existingProjectAsString) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else if (!existingProjectAsString) {
+      res.status(404).json({ error: 'Project not found' });
+    } else {
+      redis.get(`project:id:${newId}`, (err, existingProjectWithId) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else if (existingProjectWithId) {
+          res.status(409).json({ error: 'Project id already exists' });
+        } else {
+          const existingProject = JSON.parse(existingProjectAsString);
+          // update the existing project with new data
+          existingProject.projectName = projectName;
+          existingProject.description = description;
+          existingProject.image = image;
+          existingProject.content = content;
+          existingProject.slug = newSlug;
+          existingProject.id = newId;
+          // delete the old entry
+          redis.del(`project:slug:${oldSlug}`);
+          // save the updated project to Redis
+          redis.set(`project:id:${newId}`, JSON.stringify(existingProject));
+          res.json({ message: 'Project updated successfully' });
+        }
+      });
+    }
+  });
+});
+
+// need to be after PUT
+adminRouter.route('/project-update/:slug').get(async (req, res) => {
   const slug = req.params.slug;
 
   // Get the project from Redis
-  const projectAsString = await redis.get(`project:${slug}`);
+  const projectAsString = await redis.get(`project:slug:${slug}`);
   if (!projectAsString) {
     res.status(404).json({ error: 'Project not found' });
     return;
@@ -118,41 +145,19 @@ adminRouter.route('/:slug').get(async (req, res) => {
 
   // Render the project page
   const project = JSON.parse(projectAsString);
-  res.render('admin/edit-project', { project });
+
+  res.render('admin/edit-project', { user: req.user, project });
 });
 
 
-adminRouter.route('/:id/update').put(async (req, res) => {
-  const { projectName, description, image, content } = req.body;
-  const id = req.params.id;
-
-  // Get the existing project from Redis
-  const existingProjectAsString = await redis.get(`project:${id}`);
-  if (!existingProjectAsString) {
-    res.status(404).json({ error: 'Project not found' });
-    return;
-  }
-
-  // Update the existing project with the new data
-  const existingProject = JSON.parse(existingProjectAsString);
-  existingProject.projectName = projectName;
-  existingProject.description = description;
-  existingProject.image = image;
-  existingProject.content = content;
-
-  // Save the updated project to Redis
-  await redis.set(`project:${id}`, JSON.stringify(existingProject));
-
-  // Redirect the user to the updated project page
-  res.redirect(`/projects/${id}`);
-});
-
-adminRouter.route('/:id/delete').delete(async (req, res) => {
+adminRouter.route('/project-delete/:id').delete(async (req, res) => {
 
   const id = req.params.id;
   // Delete the project from Redis
-  await redis.del(`project:${id}`);
-  res.redirect('/projects');
+  await redis.del(`project:slug:${id}`);
+  // delete from project:projectnames", projectName
+  // todo
+  res.status(200).json({ message: 'Project deleted successfully', status: 'success' });
 });
 
 
